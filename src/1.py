81e12bb0b534a1a5656e86a49c1ce2541bf7ee6c
TerraMonster5 <william.dargan@gmail.com>
2024-10-09 20:04:04 +0100

    Fixed problem 1

--- src/1.py
+++ src/1.py
@@ -1,7 +1,5 @@
 total = 0
-for i in range(0, 1001):
-    if i % 3 == 0:
-        total += i
-    elif i % 5 == 0:
+for i in range(0, 1000):
+    if i % 3 == 0 or i % 5 == 0:
         total += i
 print(total)
